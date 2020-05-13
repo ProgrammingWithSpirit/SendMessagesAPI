d625b23703cc30a994b484504a1101cc9922d91d
ProgrammingWithSpirit <blogtatsiana2019@gmail.com>
2020-05-12 21:03:08 -0400

    Initial check-in of module SendMessagesApi

---
+++ Book.cs
@@ -0,0 +1,11 @@
+namespace SendMessagesApi
+{
+    public class Book
+    {
+        public string Date { get; set; }
+
+        public int PagesCount { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ Controllers/BookController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SendMessagesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly IBus _bus;
+
+        public  BookController(IBus bus)
+        {
+            _bus = bus;
+        }
+
+        [HttpPost]
+        public async void PostAsync(Book book)
+        {
+            var sendEndpoint = await _bus.GetSendEndpoint(new Uri("rabbitmq://localhost/quartz"));
+            var destinationAddress = new Uri("rabbitmq://localhost/web-service-endpoint");
+            var deliveryTime = DateTime.Now.AddSeconds(10);
+
+            await sendEndpoint.ScheduleSend(destinationAddress, deliveryTime, book);
+        }
+    }
+}
